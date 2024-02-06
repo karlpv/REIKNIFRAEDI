407a0ea07811425bd20b541d13c9c7e92778156a
Karl Philip <karlpv213@gmail.com>
2024-02-06 21:55:41 +0000

    skil 3

--- skil/skil_s3.py
+++ skil/skil_s3.py
@@ -1,21 +1,23 @@
 import re
-
+import numpy as np
 class Skil(object):
     """Skilaverkefni 3 f. REI
 
     """
 
     def arr_to_dict(self,arr):
-        
-
-
-
+        d = {}
+        for i,row in enumerate(arr):
+            for j,value in enumerate(row):
+                d[(i,j)] = value
+        return d   
 
     def dict_to_arr(self,d):
+        fylki = np.zeros
+        
+        
+    def snuavid(self,d1):
         
 
-    def snuavid(self,d1):
-
-
-    if __name__ == '__main__':
-        skil = Skil()
+if __name__ == '__main__':
+    skil = Skil()
