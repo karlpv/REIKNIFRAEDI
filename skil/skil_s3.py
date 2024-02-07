a261674d3a6121ff6d118528a857c363daba5414
Karl Philip <karlpv213@gmail.com>
2024-02-07 22:40:08 +0000

    timadaemi4

--- skil/skil_s3.py
+++ skil/skil_s3.py
@@ -13,11 +13,27 @@ class Skil(object):
         return d   
 
     def dict_to_arr(self,d):
-        fylki = np.zeros
+        staerd = max(max(key) for key in d.keys()) + 1  
         
+        ls = [[0]*staerd for i in range(staerd)]
+        
+        for (i, j), value in d.items():
+            ls[i][j] = value
+        
+        return ls
         
     def snuavid(self,d1):
-        
+        d2 = {}
+        for key, value in d1.items():
+            
+            if value in d2:
+                if not isinstance(d2[value], list):
+                    d2[value] = [d2[value]]
+                d2[value].append(key)
+            else:
+                d2[value] = key
+        return d2
+
 
 if __name__ == '__main__':
     skil = Skil()
---
+++ tima/skil_t4py.py
@@ -0,0 +1,18 @@
+import re
+import numpy as np
+class Skil(object):
+    """Timadaemi 4 f. REI
+
+    """
+    def mat1(self,...):
+        
+
+    def mat2(self,...):
+
+        return z1,z2,z3,z4
+        
+
+
+if __name__ == '__main__':
+    skil = Skil()
+
