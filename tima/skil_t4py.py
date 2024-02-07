033772ec2fc31d0d7d7d840de0d8bbf6e4b00972
Karl Philip <karlpv213@gmail.com>
2024-02-07 23:11:54 +0000

    timadaemi4

--- tima/skil_t4py.py
+++ tima/skil_t4py.py
@@ -4,13 +4,30 @@ class Skil(object):
     """Timadaemi 4 f. REI
 
     """
-    def mat1(self,...):
+    def mat1(self,m,n,types):
+        if types == "zeros" :
+            zeros = np.zeros((m,n))   
+            return zeros
+        elif types == 'ones':
+            ones = np.ones((m,n))
+            return ones
+        elif types == 'eye':
+            eye = np.eye(m,n)
+            return eye
+        elif types == 'diag':
+            diag = np.diag(np.arange(min(m, n)))
+            return diag
+        else: return None
+    def mat2(self,m1,m2,j1,j2):
+        m1_j1 = m1[:, j1]
+        m2_j2 = m2[:, j2]
         
+        z1 = m1_j1 + m2_j2  
+        z2 = m1_j1 - m2_j2  
+        z3 = m1_j1 * m2_j2  
+        z4 = np.dot(m1_j1, m2_j2)  
 
-    def mat2(self,...):
-
-        return z1,z2,z3,z4
-        
+        return z1, z2, z3, z4
 
 
 if __name__ == '__main__':
