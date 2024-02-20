a86f17d0ba8e508e5b19a824cd8435cf38d5f219
karlpv <karlpv213@gmail.com>
2024-02-20 18:22:25 +0000

    t6

--- tima/skil_t6.py
+++ tima/skil_t6.py
@@ -1,13 +1,24 @@
 import re
 import numpy as np
+import pandas as pd 
 class Skil(object):
     """Timadaemi 2 f. REI
 
     """
-    def pd_calc(self,arr,col_names,row_names):
+    def pd_calc(self,arr, col_names, row_names):
+        n, m = arr.shape 
+        if len(col_names) < m:
+            col_names += [col_names[-1]] * (m - len(col_names))
+        else:
+            col_names = col_names[:m]
         
-
-
+        if len(row_names) < n:
+            row_names += [row_names[-1]] * (n - len(row_names))
+        else:
+            row_names = row_names[:n]
+        
+        df = pd.DataFrame(arr, columns=col_names, index=row_names)
+        return df
 
 
 if __name__ == '__main__':
