c1fb33f4f1ee1dbb79ccc8696f0c4c58365ba241
karlpv <karlpv213@gmail.com>
2024-02-20 17:29:25 +0000

    add t6

---
+++ tima/skil_t6.py
@@ -0,0 +1,15 @@
+import re
+import numpy as np
+class Skil(object):
+    """Timadaemi 2 f. REI
+
+    """
+    def pd_calc(self,arr,col_names,row_names):
+        
+
+
+
+
+if __name__ == '__main__':
+    skil = Skil()
+
