e794336ed173e013a2df8ceb00fb54f0827f99ca
Karl Philip <karlpv213@gmail.com>
2024-03-13 22:12:09 +0000

    ready for s8

---
+++ skil/skil_s8.py
@@ -0,0 +1,22 @@
+import re
+import numpy as np
+
+
+class Skil(object):
+    """Skilaverkefni 8 f. REI
+
+    """
+    def gradient_descent(self,x0,df,tol,step):
+        
+
+    def f(self,x):
+
+    def df(self,x):
+
+
+
+
+
+if __name__ == '__main__':
+    skil = Skil()
+
